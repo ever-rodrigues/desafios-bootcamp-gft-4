b1cb26858189dd0dfe7450e6d0555af098364cfb
evertonrodriguesbrz <e.rodrigues1@pucpr.edu.br>
2022-02-03 11:45:14 -0300

    fix matriz code

--- DeafioGFT4/src/main/java/Desafio_GFT_C4_09_Matriz.java
+++ DeafioGFT4/src/main/java/Desafio_GFT_C4_09_Matriz.java
@@ -3,23 +3,44 @@ import java.util.Scanner;
 public class Desafio_GFT_C4_09_Matriz {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        double soma = 0;
+        float soma = 0f;
         char O = leitor.next().toUpperCase().charAt(0);
-        double[][] M = new double[12][12];
-        for(int linha=0;linha<11;linha++){
-            for(int coluna=0;coluna<11;coluna++){
-                M[linha][coluna]=leitor.nextInt();
-            }
-            for(linha=1;linha<11;linha++){
-                for(int coluna=7;coluna<=11;coluna++){
-                    if ((linha!=coluna) && (coluna-linha>0))
-                        System.out.println(M[linha][coluna]);
-                        soma+=M[linha][coluna];
-                    }
-
+        float[][] matriz = new float[12][12];
+        int linha;
+        int coluna;
+        float contador=0f;
+        for (linha = 0; linha <= 11; linha++) {
+            for (coluna = 0; coluna <= 11; coluna++) {
+                matriz[linha][coluna] = leitor.nextFloat();
+                if((linha>=1 && linha <11 && coluna==11)) {
+                    soma = soma + matriz[linha][coluna];
+                    contador++;
+                }
+                if((linha>=2&& linha<10 && coluna==10)){
+                    soma=soma+matriz[linha][coluna];
+                    contador++;
+                }
+                if((linha>=3 &&linha<9 &&coluna==9)){
+                    soma=soma+matriz[linha][coluna];
+                    contador++;
+                }
+                if((linha>=4 &&linha<8 &&coluna==8)){
+                    soma=soma+matriz[linha][coluna];
+                    contador++;
+                }
+                if((linha>=5 &&linha<7 &&coluna==7)){
+                    soma=soma+matriz[linha][coluna];
+                    contador++;
                 }
             }
+
+        }
+        if(O=='S'){
+            System.out.printf("%.1f",soma);
+        }
+        else{
+            System.out.printf("%.1f",soma/contador);
         }
 
     }
-
+}
